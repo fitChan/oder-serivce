a3f05394ebf493330f641a7076c06137728b8964
fitChan <84306157+fitChan@users.noreply.github.com>
2022-09-16 17:51:54 +0900

    Initial commit

---
+++ src/test/java/com/example/oderserivce/OderSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oderserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OderSerivceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
